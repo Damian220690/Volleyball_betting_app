4d18af38bdc1d0b657e7839e8ebfb1bc0ba63da3
Damian <Damian.nieweg@gmail.comgit config --global user.email Damian.nieweg@gmail.com>
2019-07-02 00:10:58 +0200

    betting on matches that have started or finished has been prevented

--- src/main/webapp/WEB-INF/static/js/userCoupon.js
+++ src/main/webapp/WEB-INF/static/js/userCoupon.js
@@ -4,7 +4,6 @@ $(function () {
         var deposit = $("input[type=number]").val();
         var course = $("#fullCourse").text();
         var result = deposit * course;
-        console.log(deposit);
         $('#possibleWin').val(result.toFixed(2));
     });
 
@@ -17,7 +16,41 @@ $(function () {
             $('#deposit').attr('max', depositRange);
         });
     }, 1000);
+
+
+    setInterval(function(){
+        alertStatus = false;
+        fetch('/api/matches').then(function (response) {
+            return response.json();
+        }).then(function (response) {
+            var submitButton = $("#confirmButton");
+            for (var matchProgress of response) {
+                var matchNameFromResponse = matchProgress.team1 + "-" + matchProgress.team2;
+                var inputWithMatchesName =  $('input[value="' + matchNameFromResponse + '"]');
+                inputWithMatchesName.attr('data-status', matchProgress.matchStatus);
+                var statusAttr = inputWithMatchesName.attr('data-status');
+                if(statusAttr !== undefined){
+                    inputWithMatchesName.attr('style','background-color: #264075');
+                    inputWithMatchesName.parent().attr('style','background-color: #264075');
+                    inputWithMatchesName.parent().parent().attr('style','background-color: #264075; border: 3px solid #EA1818');
+                    submitButton.click(function(){
+                        if(alertStatus === false) {
+                            alert('Accepting bets on certain events has been stopped, to continue please remove invalid matches from the coupon!!');
+                            alertStatus = true;
+                        }
+                        submitButton.attr("disabled", true);
+                    })
+                }
+                else{
+                    submitButton.attr("disabled", false);
+                }
+            }
+        });
+        }, 200);
+
+
     $("#cleaningButton").click(function(){
         $("tbody").empty();
     });
+
 });
